c7a9191f09dbc9616cfbf9ec15397dc7370fd99a
Himanshu Vaishnav <vh8246138@gmail.com>
2025-01-29 12:35:35 +0530

    payment reciept for digital fixed

--- src/modules/order/DigitalOrder.ts
+++ src/modules/order/DigitalOrder.ts
@@ -1,15 +1,13 @@
 import { Book } from "../books/Book";
 import { CartItem } from "../cart/CartItem";
-import { Customer } from "../customer/Customer";
-import { paymentTypes } from "../enums/paymentTypes";
-import { PaymentReceiptSchema } from "../payment/Payment";
+import { PaymentReceipt } from "../payment/PaymentStructure";
 import { Order } from "./Order";
 
 console.log("hii")
 
 export class DigitalOrder extends Order {
 
-    constructor( products: CartItem[], totalPriceOfOrder:number,  paymentDetail:PaymentReceiptSchema,orderType : string) {
+    constructor( products: CartItem[], totalPriceOfOrder:number,  paymentDetail:PaymentReceipt,orderType : string) {
         super(products, totalPriceOfOrder, paymentDetail,orderType);
     }
     
